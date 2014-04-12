8a2a7d0c8e9df09deb0cc4d7f47a1d6a51520d75
Jason Ray <jasonray@jayray.net>
2014-04-12 12:22:15 -0400

    extract logic into sub functions

--- app.js
+++ app.js
@@ -8,66 +8,9 @@ process.on('exit', function(code) {
 });
 
 if (cluster.isMaster) {
-	//cluster.isMaster documentation = http://nodejs.org/docs/latest/api/cluster.html#cluster_cluster_ismaster
-	l("starting master process");
-	logProcessInfo(process);
-	l("# of cpu: " + numCPUs);
-
-	// Fork workers.
-	for (var i = 0; i < numCPUs; i++) {
-		l("creating child process %d using cluster.fork ", i);
-		cluster.fork();
-	}
-
-	//register exit event, allowing us to automatically restart child process
-	cluster.on('exit', function(worker, code, signal) {
-		l("worker " + worker.process.pid + " has died [code=" + code + "][signal=" + signal + "]");
-
-		l('will restart worker in 1 second');
-		setTimeout(function() {
-			l("restarting worker");
-			cluster.fork();
-		}, 1000);
-	});
-
-	//register online, not sure what the purpose of this will be, but I want to see when this will fire
-	cluster.on('online', function(worker) {
-		l('worker ' + worker.process.pid + ' is now online');
-	});
-
-	//every 10 seconds, have the master report what processes it thinks it is running
-	setInterval(function() {
-		//check on workers
-		for (var id in cluster.workers) {
-			var worker = cluster.workers[id];
-			l("check on worker " + id + "; pid " + worker.process.pid);
-		}
-	}, 10000);
+	startCluster(cluster);
 } else if (cluster.isWorker) {
-	//cluster.isWorker documentation = http://nodejs.org/docs/latest/api/cluster.html#cluster_cluster_isworker
-	var randomId = Math.floor((Math.random() * 1000) + 1);
-	l("starting worker " + randomId);
-
-	var iT = 0;
-	var iI = 0;
-
-	setTimeout(function() {
-		iT = iT + 1;
-		l("timer fired " + iT);
-	}, 10000);
-
-	setInterval(function() {
-		iI = iI + 1;
-		l("interval fired " + iI);
-	}, 1000);
-
-	setInterval(function() {
-		logMemoryUsage(process);
-	}, 5000);
-
-	setInterval(function() {
-		runExpensiveOperation();
-	}, 30000);
+	startWorker(cluster);
 } else {
 	l("not a master, not a worker??");
 }
@@ -103,4 +46,70 @@ function runExpensiveOperation() {
 		}
 	}
 	l('end expensive operation');
+}
+
+
+
+function startCluster(cluster) {
+	//cluster.isMaster documentation = http://nodejs.org/docs/latest/api/cluster.html#cluster_cluster_ismaster
+	l("starting master process");
+	logProcessInfo(process);
+	l("# of cpu: " + numCPUs);
+
+	// Fork workers.
+	for (var i = 0; i < numCPUs; i++) {
+		l("creating child process %d using cluster.fork ", i);
+		cluster.fork();
+	}
+
+	//register exit event, allowing us to automatically restart child process
+	cluster.on('exit', function(worker, code, signal) {
+		l("worker " + worker.process.pid + " has died [code=" + code + "][signal=" + signal + "]");
+
+		l('will restart worker in 1 second');
+		setTimeout(function() {
+			l("restarting worker");
+			cluster.fork();
+		}, 1000);
+	});
+
+	//register online, not sure what the purpose of this will be, but I want to see when this will fire
+	cluster.on('online', function(worker) {
+		l('worker ' + worker.process.pid + ' is now online');
+	});
+
+	//every 10 seconds, have the master report what processes it thinks it is running
+	setInterval(function() {
+		//check on workers
+		for (var id in cluster.workers) {
+			var worker = cluster.workers[id];
+			l("check on worker " + id + "; pid " + worker.process.pid);
+		}
+	}, 10000);
+}
+
+function startWorker(cluster) {
+	//cluster.isWorker documentation = http://nodejs.org/docs/latest/api/cluster.html#cluster_cluster_isworker
+	l("starting worker ");
+
+	var iT = 0;
+	var iI = 0;
+
+	setTimeout(function() {
+		iT = iT + 1;
+		l("timer fired " + iT);
+	}, 10000);
+
+	setInterval(function() {
+		iI = iI + 1;
+		l("interval fired " + iI);
+	}, 1000);
+
+	setInterval(function() {
+		logMemoryUsage(process);
+	}, 5000);
+
+	setInterval(function() {
+		runExpensiveOperation();
+	}, 30000);
 }
