353de322167b5d494216374257f65b304e1dd829
Jason Ray <jasonray@jayray.net>
2014-04-12 12:28:56 -0400

    formatting

--- app.js
+++ app.js
@@ -15,41 +15,6 @@ if (cluster.isMaster) {
 	l("not a master, not a worker??");
 }
 
-function l(message) {
-	console.log("[" + process.pid + "] " + message);
-}
-
-function logProcessInfo(p) {
-	// documentation on the process object: http://nodejs.org/api/process.html
-	l('pid: ' + p.pid);
-	l('argv: ' + p.argv);
-	l('execPath: ' + p.execPath);
-	l('execArgv: ' + p.execArgv);
-	l('version: ' + p.version);
-	l('versions: ' + p.versions);
-	l('config: ' + p.config);
-	l('title: ' + p.title);
-	l('platform: ' + p.platform);
-	l('uptime: ' + p.uptime());
-}
-
-function logMemoryUsage(p) {
-	l("memory for " + p.pid + ": " + util.inspect(p.memoryUsage()));
-}
-
-function runExpensiveOperation() {
-	l('start expensive operation');
-	for (var j = 0; j < 30; j++) {
-		var a = [];
-		for (var i = 0; i < 10000000; i++) {
-			a.push(i);
-		}
-	}
-	l('end expensive operation');
-}
-
-
-
 function startCluster(cluster) {
 	//cluster.isMaster documentation = http://nodejs.org/docs/latest/api/cluster.html#cluster_cluster_ismaster
 	l("starting master process");
@@ -92,24 +57,64 @@ function startWorker(cluster) {
 	//cluster.isWorker documentation = http://nodejs.org/docs/latest/api/cluster.html#cluster_cluster_isworker
 	l("starting worker ");
 
+	// run this timer event, one time, to demonstrate that iT (number of times that this timer has fired)
+	// is a variable local to this worker, since each worker is its own process	
 	var iT = 0;
-	var iI = 0;
-
 	setTimeout(function() {
 		iT = iT + 1;
 		l("timer fired " + iT);
 	}, 10000);
 
+	// run this interval event, every 1 second, to demonstrate that iI (number of times that this timer has fired)
+	// is a variable local to this worker, since each worker is its own process	
+	var iI = 0;
 	setInterval(function() {
 		iI = iI + 1;
 		l("interval fired " + iI);
 	}, 1000);
 
+	// output memory information about this process every min
+	// this would be a good thing to have the master report on the worker process
 	setInterval(function() {
 		logMemoryUsage(process);
-	}, 5000);
+	}, 60000);
 
-	setInterval(function() {
-		runExpensiveOperation();
-	}, 30000);
+	// every 30 seconds run an expensive CPU-bound operation
+	// setInterval(function() {
+	// 	runExpensiveOperation();
+	// }, 30000);
+}
+
+function runExpensiveOperation() {
+	l('start expensive operation');
+	for (var j = 0; j < 30; j++) {
+		var a = [];
+		for (var i = 0; i < 10000000; i++) {
+			a.push(i);
+		}
+	}
+	l('end expensive operation');
+}
+
+// i created this log wrapper simply so that I can add the process info to the message
+function l(message) {
+	console.log("[%s] %s", process.pid, message);
+}
+
+function logProcessInfo(p) {
+	// documentation on the process object: http://nodejs.org/api/process.html
+	l('pid: ' + p.pid);
+	l('argv: ' + p.argv);
+	l('execPath: ' + p.execPath);
+	l('execArgv: ' + p.execArgv);
+	l('version: ' + p.version);
+	l('versions: ' + p.versions);
+	l('config: ' + p.config);
+	l('title: ' + p.title);
+	l('platform: ' + p.platform);
+	l('uptime: ' + p.uptime());
+}
+
+function logMemoryUsage(p) {
+	l("memory for " + p.pid + ": " + util.inspect(p.memoryUsage()));
 }
